66ab489224106a2b1598ec9c7a3a0b5f07e2ffd1
bobik1234 <rafal_piotrowski1@op.pl>
2018-05-09 16:15:50 +0200

    Update settings.py

--- bets/settings.py
+++ bets/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'registration',
+    'bets.registration',
     'tournament'
 ]
 
