39059c2cc29ac3109adba848e67e99a6e9d7f46f
bobik1234 <rapi1978@gmail.com>
2018-07-26 16:32:30 +0200

    merge

--- bets/settings.py
+++ bets/settings.py
@@ -127,8 +127,8 @@ STATIC_ROOT = "/home/bobik1234/bobik1234.pythonanywhere.com/static"
 LOGIN_REDIRECT_URL = '/admin/'
 ACCOUNT_ACTIVATION_DAYS = 2 # two days
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
