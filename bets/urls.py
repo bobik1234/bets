90705b3f0599a28a6ba5ed7c3f0740082b99d453
bobik1234 <rapi1978@gmail.com>
2018-12-06 10:55:29 +0100

    merge

--- bets/urls.py
+++ bets/urls.py
@@ -1,29 +1,34 @@
-from django.conf.urls import url, include
 from django.contrib import admin
 from django_registration.forms import RegistrationFormUniqueEmail
 from django_registration.backends.activation.views import RegistrationView
 from django.contrib.auth import views as auth_views
 from tournament.views import EmailChange, EmailChangeDone, PasswordChange, PasswordChangeDone, Logout
+from django.urls import path, re_path, include
+
 
 urlpatterns = [
-    url(r'^$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^tournament/', include('tournament.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^logout/', Logout.as_view(), name='logout'),
-    url(r'^password_reset_done/$', auth_views.password_reset_done,
-            {'template_name': 'registration/reset_password_done.html'}, name='password_reset_done'),
-    url(r'^password_reset/$', auth_views.password_reset,
-            {'template_name': 'registration/reset_password_form.html',
-             'email_template_name' : 'registration/reset_password_email.html'}, name='password_reset'),
-    url(r'^password_change_done/$', PasswordChangeDone.as_view(), name='change_password_done'),
-    url(r'^password_change/$', PasswordChange.as_view(), name='change_password'),
-    url(r'^email_change/$', EmailChange.as_view(), name='change_email'),
-    url(r'^email_change_done/$', EmailChangeDone.as_view(), name='change_email_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,
-        {'template_name': 'registration/reset_password_confirm.html'}, name='password_reset_confirm'),
-    url(r'^reset_done/$', auth_views.password_reset_complete,
-            {'template_name': 'registration/reset_password_complete.html'}, name='password_reset_complete'),
-    url(r'^accounts/register/$',RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
+    re_path(r'^$', auth_views.LoginView.as_view(), name='login'),
+    path('tournament/', include('tournament.urls')),
+    path('admin/', admin.site.urls),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('password_reset_done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_password_done.html'),
+        name='password_reset_done'),
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(template_name='registration/reset_password_form.html',
+                                             email_template_name='registration/reset_password_email.html'),
+        name='password_reset'),
+    path('password_change_done/', PasswordChangeDone.as_view(), name='change_password_done'),
+    path('password_change/', PasswordChange.as_view(), name='change_password'),
+    path('email_change/', EmailChange.as_view(), name='change_email'),
+    path('email_change_done/', EmailChangeDone.as_view(), name='change_email_done'),
+    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset_password_confirm.html'),
+            name='password_reset_confirm'),
+    path('reset_done/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_password_complete.html'),
+        name='password_reset_complete'),
+    path('accounts/register/',RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
         name='registration_register'),
-    url(r'^accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django_registration.backends.activation.urls')),
 ]
